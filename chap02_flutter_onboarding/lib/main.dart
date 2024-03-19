8881da196cbfbbcf8291727229758735518d1447
EuniceYou12 <euni.you@gmail.com>
2024-03-19 10:07:53 +0900

    font

--- chap02_flutter_onboarding/lib/main.dart
+++ chap02_flutter_onboarding/lib/main.dart
@@ -1,74 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:intro_screen_onboarding_flutter/intro_app.dart';
+import 'package:intro_screen_onboarding_flutter/introduction.dart';
+import 'package:intro_screen_onboarding_flutter/introscreenonboarding.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-//SharedPreferences 인스턴스를 어디서든 접근 가능하도록 전역 변수로 선언
-// late : 나중에 꼭 값을 할당 해준다는 의미.
+// SharedPreferences 인스턴스를 어디서든 접근 가능하도록 전역 변수로 선언
+// late : 나중에 꼭 값을 할당해준다는 의미.
 late SharedPreferences prefs;
 
 void main() async {
-  // main() 함수에서 async 쓰려면 필요
+  // main()함수에서 async를 쓰려면 필요
   WidgetsFlutterBinding.ensureInitialized();
 
   //Shared_preferences 인스턴스 생성
   prefs = await SharedPreferences.getInstance();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // SharedPreference 에서 온보딩 완료 여부 조회
+    // SharedPreferences 에서 온보딩 완료 여부 조회
     // isOnboarded에 해당하는 값에서 null을 반환하는 경우 false를 기본값으로 지정.
     bool isOnboarded = prefs.getBool('isOnboarded') ?? false;
 
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.black26,
+        fontFamily: "Pretendard",
       ),
+      title: 'Who Am I',
+      // isOnboarded 값에 따라 Homepage로 열지 TestScreen으로 열지 결정됨.
       home: isOnboarded ? HomePage() : TestScreen(),
     );
   }
 }
 
 class TestScreen extends StatelessWidget {
-  final List<Introduction> introductionList = [
+  final List<Introduction> list = [
     Introduction(
       title: '현재의 나',
-      subTitle: '아직 초보 개발자 이지만\n 아기 오리처럼 열심히 노력해서 \n배우고 나아가고 싶습니다',
-      imageUrl: 'assets/images/b.jpg',
+      titleTextStyle: TextStyle(
+          color: Colors.purple,
+          fontSize: 35,
+          fontFamily: "Pretendard",
+          fontWeight: FontWeight.w800),
+      subTitle: 'ENFJ 저는 오지잪이 넓은 사람입니다. \n 제 문제는 몰라도 여러분에겐 \n 든든한 강사가 되겠습니다.',
+      subTitleTextStyle: TextStyle(
+        fontSize: 25,
+        fontFamily: "WagleWagle",
+      ),
+      imageUrl: 'assets/images/before.png',
     ),
     Introduction(
       title: '수료 후의 나',
-      subTitle:
-          '멋진 백조의 모습처럼\n우아하고여유로운 모습을 하고\n수면아래 백조 다리처럼\n열심히 일을 하고 있을것 같습니다',
-      imageUrl: 'assets/images/c.jpg',
+      titleTextStyle: TextStyle(color: Colors.purple, fontSize: 35),
+      subTitle: '여러분은 취업 할 수 있어요!',
+      imageUrl: 'assets/images/after.png',
     ),
     Introduction(
       title: '10년 후의 나',
-      subTitle: '별이 보이는 집을 지어서\n가족과 함께 생활 하며\n여유로운 개발을 하고 싶습니다',
-      imageUrl: 'assets/images/d.jpg',
+      titleTextStyle: TextStyle(color: Colors.purple, fontSize: 35),
+      subTitle: '경치 좋은 캠핑장 텐트 안에서\n 개발중..ing',
+      imageUrl: 'assets/images/ten_years_ago.png',
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return IntroScreenOnboarding(
-      introductionList: introductionList,
+      introductionList: list,
       onTapSkipButton: () {
-        //마지막페이지가 나오거나 skip을 해서 Homepage로 가기전에 isOnboard를 true로 바꿔준다.
+        // 마지막 페이지가 나오거나 skip을 해서 Homepage로 가기전에 isOnboarded를 true로 바꿔준다.
         prefs.setBool('isOnboarded', true);
         Navigator.push(
           context,
           MaterialPageRoute(
             builder: (context) => HomePage(),
-          ),
+          ), //MaterialPageRoute
         );
       },
     );
@@ -76,8 +87,6 @@ class TestScreen extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -91,11 +100,10 @@ class HomePage extends StatelessWidget {
       ]),
       body: Center(
         child: Text(
-          'Welcome to Home Page!',
+          'Welcome to Home Page! \n 가나다라마바사',
           style: TextStyle(
             fontSize: 24.0,
             fontWeight: FontWeight.bold,
-            color: Colors.lightBlueAccent,
           ),
         ),
       ),
--- chap02_flutter_onboarding/test/widget_test.dart
+++ chap02_flutter_onboarding/test/widget_test.dart
@@ -1,30 +1,79 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:chap02_flutter_onboarding/main.dart';
+import 'package:intro_screen_onboarding_flutter/introduction.dart';
+import 'package:intro_screen_onboarding_flutter/introscreenonboarding.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  runApp(const MyApp());
+}
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Who Am I',
+      home: TestScreen(),
+    );
+  }
+}
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+class TestScreen extends StatelessWidget {
+  final List<Introduction> list = [
+    Introduction(
+      title: '현재의 나',
+      titleTextStyle: TextStyle(color: Colors.purple, fontSize: 35),
+      subTitle: 'ENFJ 저는 오지잪이 넓은 사람입니다. \n 제 문제는 몰라도 여러분에겐 \n 든든한 강사가 되겠습니다.',
+      imageUrl: 'assets/images/before.png',
+    ),
+    Introduction(
+      title: '수료 후의 나',
+      titleTextStyle: TextStyle(color: Colors.purple, fontSize: 35),
+      subTitle: '여러분은 취업 할 수 있어요!',
+      imageUrl: 'assets/images/after.png',
+    ),
+    Introduction(
+      title: '10년 후의 나',
+      titleTextStyle: TextStyle(color: Colors.purple, fontSize: 35),
+      subTitle: '경치 좋은 캠핑장 텐트 안에서\n 개발중..ing',
+      imageUrl: 'assets/images/ten_years_ago.png',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroScreenOnboarding(
+      introductionList: list,
+      onTapSkipButton: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomePage(),
+          ), //MaterialPageRoute
+        );
+      },
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Home Page!',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
 }
